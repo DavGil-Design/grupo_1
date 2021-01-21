acfb623313da7c15a046dd47a298d82ee2d8a007
DavGil-Design <davids@gilsbg@gmail.com>
2021-01-21 17:45:59 -0500

    Master add start in app.js and package.json

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const path =require('path');
+const path = require('path');
 
-const app = express ();
+const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
+app.use('/static', express.static(__dirname+'/public'));
 
-app.lsiten(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000')
-});
+app.listen(3080,()=>{
+    console.log("servidor 3080 funcionando");
+})
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
-});
+app.get("/",(req,res)=>{
+    pathHtml=path.join(__dirname,"/views/home.html");
+    res.sendFile(pathHtml);
+})
