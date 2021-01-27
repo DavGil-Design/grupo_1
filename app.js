63b0146a14aed476b2e4893c8f5e3a4bf55dd8d9
AndyTheCruckl <andyboce@hotmail.com>
2021-01-27 14:04:36 -0600

    Adding login.html and modifiying app.js and the fonts folder

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const path = require('path');
 
 const app = express();
 
-app.use('/static', express.static(__dirname+'/public'));
+app.use('/static', express.static(__dirname+'/src/public'));
 
-app.listen(3080,()=>{
-    console.log("servidor 3080 funcionando");
-})
+
+app.listen(3080, () => console.log("servidor 3080 funcionando"));
 
 app.get("/",(req,res)=>{
-    pathHtml=path.join(__dirname,"/views/home.html");
-    res.sendFile(pathHtml);
-})
+    res.sendFile(path.join(__dirname, "/src/views/home.html")
+)});
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/src/views/register.html")
+)});
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/src/views/login.html")
+)});
